0d2533567f02c0e4427f8d16648fa51d0d11946d
dani <dani@192.168.1.128>
2017-01-29 11:23:50 +0100

    Populate database

---
+++ mywebapp-data/src/main/resources/sql/createTables.sql
@@ -0,0 +1,124 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mytest
+-- ------------------------------------------------------
+-- Server version	5.7.17-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `estudiante`
+--
+
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `estudiante` (
+  `id` bigint(20) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `apellido` varchar(100) NOT NULL,
+  `nivelDeEstudios` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `estudiante`
+--
+
+LOCK TABLES `estudiante` WRITE;
+/*!40000 ALTER TABLE `estudiante` DISABLE KEYS */;
+INSERT INTO `estudiante` VALUES (1,'sdfasd','asdf asdfasd fasd','Bachillerato'),(2,'dani2','acentó','Bachillerato'),(50,'rrerwe','asdfas asd fasdf','Secundaria'),(100,'asdfas','asdfasdf sd','Primaria');
+/*!40000 ALTER TABLE `estudiante` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `prueba`
+--
+
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `prueba` (
+  `ID` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `prueba`
+--
+
+LOCK TABLES `prueba` WRITE;
+/*!40000 ALTER TABLE `prueba` DISABLE KEYS */;
+/*!40000 ALTER TABLE `prueba` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sequences`
+--
+
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sequences` (
+  `seq_name` varchar(100) NOT NULL,
+  `seq_count` bigint(20) NOT NULL,
+  PRIMARY KEY (`seq_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sequences`
+--
+
+LOCK TABLES `sequences` WRITE;
+/*!40000 ALTER TABLE `sequences` DISABLE KEYS */;
+INSERT INTO `sequences` VALUES ('id',3),('seq_estu',0);
+/*!40000 ALTER TABLE `sequences` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `version`
+--
+
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `version` (
+  `version` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `version`
+--
+
+LOCK TABLES `version` WRITE;
+/*!40000 ALTER TABLE `version` DISABLE KEYS */;
+/*!40000 ALTER TABLE `version` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'mytest'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-01-29 10:59:02
--- mywebapp/src/main/java/my/project/mywebapp/config/Dbconfig.java
+++ mywebapp/src/main/java/my/project/mywebapp/config/Dbconfig.java
@@ -1,22 +1,60 @@
 package my.project.mywebapp.config;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 
-public class Dbconfig  implements ApplicationListener<ContextRefreshedEvent>{
+public class Dbconfig implements ApplicationListener<ContextRefreshedEvent> {
+	@Autowired
+	private ApplicationContext appContext;
 
-	private static boolean daosCargados;
+	private static boolean dbActualizada;
+
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event) {
-		if (!daosCargados){
-			System.out.println("Me ejecuto al arrancar!!");
-			this.daosCargados = true;
-		}else{
+		if (!dbActualizada) {
+			// cargamos el dataSource para abrir una conexion
+			DataSource dataSource = (DataSource) appContext.getBean("dataSource");
+			try {
+				//si no existe la tabla version ejecutamos script de creacion de tablas
+				if (!isTableCreated("version", dataSource.getConnection())) {
+					System.out.println("Creando tablas....");
+					ClassPathResource resource = new ClassPathResource("/sql/createTables.sql");
+					ScriptUtils.executeSqlScript(dataSource.getConnection(), new EncodedResource(resource, "UTF-8"));
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			this.dbActualizada = true;
+
+		} else {
 			System.out.println("No ejecuto nada");
 		}
-		
-		
+
 	}
-	
+
+	private boolean isTableCreated(String tableName, Connection connection) throws SQLException {
+		DatabaseMetaData meta = connection.getMetaData();
+		ResultSet res = meta.getTables(null, null, tableName, new String[] { "TABLE" });
+		return res.first();
+//		return res.getString("TABLE_NAME").equalsIgnoreCase(tableName);
+//		while (res.next()) {
+//			System.out.println("   " + res.getString("TABLE_CAT") + ", " + res.getString("TABLE_SCHEM") + ", "
+//					+ res.getString("TABLE_NAME") + ", " + res.getString("TABLE_TYPE") + ", "
+//					+ res.getString("REMARKS"));
+//		}
+	//return true;
+	}
 
 }
